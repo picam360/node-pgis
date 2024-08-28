d5b143f3438ccc592b46438f899f9bb5735cfce6
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-08-28 13:21:34 +0900

    offscreen.js

--- offscreen.js
+++ offscreen.js
@@ -4,22 +4,30 @@ const path = require('path');
 (async () => {
   // Puppeteerを起動
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+    args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process'
+    ],
     executablePath: '/usr/bin/chromium-browser' // ここにChromiumのパスを指定
   });
 
   const page = await browser.newPage();
 
   // ローカルのindex.htmlファイルを読み込む
-  const filePath = path.resolve(__dirname, 'www/index.html');
-  const fileUrl = `file://${filePath}`;
-  await page.goto(fileUrl);
+  const url = `http://localhost:9001/index.html`;
+  await page.goto(url);
 
   // スクリーンキャプチャを取得して保存
-  //setInterval(() => {
+  setInterval(async () => {
     await page.screenshot({ path: 'screenshot.png' });
-  //})
+  }, 1000)
 
   // Puppeteerを終了
-  await browser.close();
+  //await browser.close();
 })();
